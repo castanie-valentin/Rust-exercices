faa75cd86ec4390fcf42e5fb4122ea36b7b35fc6
Valentin Castanie <Castanie.valentin@gmail.com>
2023-09-26 14:51:43 +0200

    oddities

---
+++ oddities/target/debug/deps/oddities.d
@@ -0,0 +1,5 @@
+C:\projets\Rust-exercices\oddities\target\debug\deps\oddities.exe: src\main.rs
+
+C:\projets\Rust-exercices\oddities\target\debug\deps\oddities.d: src\main.rs
+
+src\main.rs:
---
+++ oddities/target/debug/oddities.d
@@ -0,0 +1,1 @@
+C:\projets\Rust-exercices\oddities\target\debug\oddities.exe: C:\projets\Rust-exercices\oddities\src\main.rs
